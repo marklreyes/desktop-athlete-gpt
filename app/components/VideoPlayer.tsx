d03a0aa418435c4a072428121c0a938dee6037b0
Mark Reyes <mark.l.reyes@gmail.com>
2025-04-06 21:24:38 -0700

    Add Video Player Component

---
+++ app/components/VideoPlayer.tsx
@@ -0,0 +1,54 @@
+import YouTube from "react-youtube";
+import { useTheme } from "../context/ThemeContext";
+import { trackEvent } from "~/utils/trackEvent";
+
+export function VideoPlayer({ videoUrl, title }: { videoUrl: string; title: string }) {
+	const { theme } = useTheme();
+
+	// Extract video ID from URL
+	const getYouTubeVideoId = (url: string): string => {
+	  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+	  const match = url.match(regExp);
+	  return (match && match[2].length === 11) ? match[2] : '';
+	};
+
+	const videoId = getYouTubeVideoId(videoUrl);
+
+	return (
+	  <div className={`w-full h-0 pb-[56.25%] relative border-[${theme.primary}]`}>
+		<YouTube
+		  videoId={videoId}
+		  className="absolute top-0 left-0 w-full h-full"
+		  opts={{
+			width: '100%',
+			height: '100%'
+		  }}
+		  onEnd={() => {
+			trackEvent("video_end", {
+			  params: {
+				video_title: title,
+				video_url: videoUrl
+			  }
+			})();
+		}}
+		  onPause={() => {
+			trackEvent("video_pause", {
+			  params: {
+
+				video_title: title,
+				video_url: videoUrl
+			  }
+			})();
+		}}
+		  onPlay={() => {
+			trackEvent("video_play", {
+			  params: {
+				video_title: title,
+				video_url: videoUrl
+			  }
+			})();
+		  }}
+		/>
+	  </div>
+	);
+  }
